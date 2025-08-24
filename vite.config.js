da7a7f7cb30e6b4bf27a4e771e3c15b2a355f743
rohmathidaya <rohmathidayatyusuf@gmail.com>
2025-08-24 09:37:59 +0700

    build vite2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: "public/build", // hasil build ke public/build
+        manifest: true, // penting! Laravel butuh ini
+        emptyOutDir: true,
+    },
 });
